91de07f6214bf6546f9c7c2e09a2a9cc4ef89ddc
Russell Mays <russell.c.mays@gmail.com>
2021-01-17 19:14:14 -0800

    static site export

--- project.clj
+++ project.clj
@@ -1,13 +1,14 @@
 (defproject russellmays-com "0.1.0-SNAPSHOT"
-  :description "russellmays.com source"
-  :url "https://russellmays.com"
-  :license {:name "MIT"
-            :url "https://opensource.org/licenses/MIT"}
+  :description  "russellmays.com source"
+  :url          "https://russellmays.com"
+  :license      {:name "MIT"
+                 :url "https://opensource.org/licenses/MIT"}
   :dependencies [[org.clojure/clojure "1.10.1"]
                  [stasis "2.5.0"]
                  [ring "1.8.2"]
                  [hiccup "1.0.5"]
                  [me.raynes/cegdown "0.1.1"]
                  [optimus "0.20.2"]]
-  :ring {:handler russellmays-com.web/app}
-  :profiles {:dev {:plugins [[lein-ring "0.12.5"]]}})
+  :ring         {:handler russellmays-com.web/app}
+  :aliases      {"build-site" ["run" "-m" "russellmays-com.web/export"]}
+  :profiles     {:dev {:plugins [[lein-ring "0.12.5"]]}})
--- src/russellmays_com/web.clj
+++ src/russellmays_com/web.clj
@@ -3,6 +3,7 @@
             [hiccup.page :refer [html5]]
             [me.raynes.cegdown :as md]
             [optimus.assets :as assets]
+            [optimus.export :as o.export]
             [optimus.optimizations :as optimizations]
             [optimus.prime :as optimus]
             [optimus.strategies :refer [serve-live-assets]]
@@ -43,14 +44,13 @@
     :markdown (markdown-pages (stasis/slurp-directory "resources/md" #"\.md$"))}))
 
 (defn ^:private wrap-pages
-  ""
-  []
-  (let [pages (get-pages)]
-    (zipmap (keys pages)
-            (map #(fn [req] %) (vals pages)))))
+  "Lazy page loading with page request fn shim."
+  [pages]
+  (zipmap (keys pages)
+          (map #(fn [req] (% req)) (vals pages))))
 
 (defn ^:private get-assets
-  ""
+  "Pulls in assets."
   []
   (concat
    (assets/load-assets "public" ["/style.css"])
@@ -58,8 +58,20 @@
                                  "/avatar-scaled.png"])))
 
 (def app
-  ""
-  (optimus/wrap (stasis/serve-pages wrap-pages)
+  "Orchestrates pages and assets for local development."
+  (optimus/wrap (-> (get-pages)
+                    wrap-pages
+                    stasis/serve-pages)
                 get-assets
                 optimizations/all
                 serve-live-assets))
+
+(defn export
+  "Builds pages and assets for production deployment."
+  []
+  (let [export-dir "dist"
+        assets     (optimizations/all (get-assets) {})
+        assets     (remove #(:outdated %) assets)]
+    (stasis/empty-directory! export-dir)
+    (o.export/save-assets assets export-dir)
+    (stasis/export-pages (get-pages) export-dir {:optimus-assets assets})))
